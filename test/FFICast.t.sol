23a16e19d0134d0ba2ed7e09dc67dbcedd585abd
Joe Jordan <dev@ignitesoft.com>
2022-12-22 17:46:12 -0500

    test: FFICast tests

---
+++ test/FFICast.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import { console2 } from "forge-std/console2.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+
+import { BytesLib } from "solidity-bytes-utils/BytesLib.sol";
+import { CramBit } from "crambit/CramBit.sol";
+
+import { FFICast } from "src/FFICast.sol";
+
+/// @notice basic functionality test of our FFICast project
+contract FFICastTest is PRBTest {
+    FFICast public ffi;
+
+    function setUp() public {
+        ffi = new FFICast();
+    }
+
+    /// @notice assert that the FFI private key generator returns a bytes32 > 0
+    function testGetRandomPrivateKey() public {
+        bytes32 pk = ffi.generatePrivateKey();
+        assertGt(uint256(pk), 0);
+    }
+
+    /// @notice output 10 randomly generated private keys
+    function testConsole10PrivateKeys() public {
+        for (uint256 i = 0; i < 10; i++) {
+            console2.logBytes32(ffi.generatePrivateKey());
+        }
+    }
+}
